2e1629ba6fb517d0a73a3c4a4f4b42a9accfade6
Ronnie Wang <geniusxiaoguai@gmail.com>
2019-12-24 15:42:06 +0800

    fix: scrapy.log deprecation

--- bgm/pipelines.py
+++ bgm/pipelines.py
@@ -4,7 +4,7 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-from scrapy import log
+
 from scrapy.exceptions import DropItem
 from twisted.enterprise import adbapi
 from scrapy import signals
--- bgmapi/pipelines.py
+++ bgmapi/pipelines.py
@@ -4,7 +4,7 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-from scrapy import log
+
 from scrapy import signals
 from scrapy.exporters import CsvItemExporter
 import datetime
